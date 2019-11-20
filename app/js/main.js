ced0736fdb95fe6a81bfb629738294f1562d502f
Prytkin Artem <prytkinart@mail.ru>
2019-11-20 16:18:20 +0400

    первая версия selection

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
 $(function(){
+
   $('#selection-choose').on('click', function() {
     $('.selection-inner__result').toggleClass('active');
+    $('.selection-inner__calc').toggleClass('active2');
   });
+
 });
