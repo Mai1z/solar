f519674d2b609e5d9ec9c4f4b25113c4d2d1da5a
Prytkin Artem <prytkinart@mail.ru>
2019-11-22 17:37:30 +0400

    добавил выезжающее мобильное меню

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,30 @@
 $(function(){
 
+  // ===== Калькулятор =====
+
   $('#selection-choose').on('click', function() {
     $('.selection-calc__result').toggleClass('active');
     $('.selection__calc').toggleClass('active2');
   });
 
+  // ===== Мобильное меню =====  
+
   $('.header-mob__burg-btn').on('click', function() {
     $('.header-mob__burg').toggleClass('activeburg');
   });
+
+  $('.header-mob__burg-btn').on('click', function() {
+    $('.header-wrapper').toggleClass('activewrap');
+    $('.header-wrapper__btn').toggleClass('closewrap');
+  });
+
+  $('.header-wrapper__btn').on('click', function() {
+    $('.header-wrapper__btn').toggleClass('closewrap');
+    $('.header-wrapper').toggleClass('activewrap');
+    $('.header-mob__burg').toggleClass('activeburg');
+  });
+
+  // ===== Списки футера =====  
 
   $('.footer__wrap').on('click', function() {
     $(this).toggleClass('activefoot');
@@ -18,4 +35,6 @@ $(function(){
   });
 
 });
+
+
 
