cb04f21e7f2027a863bea4f9fb8164d59933934d
Prytkin Artem <prytkinart@mail.ru>
2019-11-25 13:01:35 +0400

    Доработал мобильное меню и ошибки на сайте

--- app/js/main.js
+++ app/js/main.js
@@ -16,12 +16,21 @@ $(function(){
   $('.header-mob__burg-btn').on('click', function() {
     $('.header-wrapper').toggleClass('activewrap');
     $('.header-wrapper__btn').toggleClass('closewrap');
+    $('body').toggleClass('fixed');
   });
 
   $('.header-wrapper__btn').on('click', function() {
     $('.header-wrapper__btn').toggleClass('closewrap');
     $('.header-wrapper').toggleClass('activewrap');
     $('.header-mob__burg').toggleClass('activeburg');
+    $('body').toggleClass('fixed');
+  });
+
+  // ===== Действия в моб. меню =====
+
+  $('.catalog__btn').on('click', function() {
+    $('.header-wrapper__inner').toggleClass('show');
+    $('.header-wrapper__catalog').toggleClass('show');
   });
 
   // ===== Списки футера =====  
