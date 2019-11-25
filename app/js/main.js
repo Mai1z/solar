ef46cd25649e191108ee951bb91fe66ec0a735d2
Prytkin Artem <prytkinart@mail.ru>
2019-11-25 15:18:19 +0400

    Доработал мобильное меню

--- app/js/main.js
+++ app/js/main.js
@@ -1,48 +1,42 @@
-$(function(){
 
-  // ===== Калькулятор =====
+// ===== Калькулятор =====
 
-  $('#selection-choose').on('click', function() {
-    $('.selection-calc__result').toggleClass('active');
-    $('.selection__calc').toggleClass('active2');
-  });
+$('#selection-choose').on('click', function() {
+	$('.selection-calc__result').toggleClass('active');
+	$('.selection__calc').toggleClass('active2');
+});
 
-  // ===== Мобильное меню =====  
+// ===== Мобильное меню =====  
 
-  $('.header-mob__burg-btn').on('click', function() {
-    $('.header-mob__burg').toggleClass('activeburg');
-  });
+function toggleMenu() {
+	$('.header-wrapper__btn').toggleClass('closewrap');
+	$('.header-mobmenu').toggleClass('activewrap');
+	$('.header-mob__burg').toggleClass('activeburg');
+	$('body').toggleClass('fixed');
+}
 
-  $('.header-mob__burg-btn').on('click', function() {
-    $('.header-wrapper').toggleClass('activewrap');
-    $('.header-wrapper__btn').toggleClass('closewrap');
-    $('body').toggleClass('fixed');
-  });
+$('.header-mob__burg-btn, .header-wrapper__btn, .header-mobmenu').on('click', toggleMenu);
+$('.header-wrapper').on('click', function(e) {
+	e.stopPropagation();
+	return 0;
+});
 
-  $('.header-wrapper__btn').on('click', function() {
-    $('.header-wrapper__btn').toggleClass('closewrap');
-    $('.header-wrapper').toggleClass('activewrap');
-    $('.header-mob__burg').toggleClass('activeburg');
-    $('body').toggleClass('fixed');
-  });
 
-  // ===== Действия в моб. меню =====
+// ===== Действия в моб. меню =====
 
-  $('.catalog__btn').on('click', function() {
-    $('.header-wrapper__inner').toggleClass('show');
-    $('.header-wrapper__catalog').toggleClass('show');
-  });
+$('.catalog__btn').on('click', function() {
+	$('.header-wrapper__inner').toggleClass('show');
+	$('.header-wrapper__catalog').toggleClass('show');
+});
 
-  // ===== Списки футера =====  
+// ===== Списки футера =====  
 
-  $('.footer__wrap').on('click', function() {
-    $(this).toggleClass('activefoot');
-  });
+$('.footer__wrap').on('click', function() {
+	$(this).toggleClass('activefoot');
+});
 
-  $('.footer__item').on('click', function() {
-    $(this).toggleClass('activefoot');
-  });
-
+$('.footer__item').on('click', function() {
+	$(this).toggleClass('activefoot');
 });
 
 
